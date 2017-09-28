511ee8239c2c48960d4fd0ffe773415f4a2cb5e0
Ross Petrenko <petrenko.rostislav@gmail.com>
2017-09-28 18:43:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  return str.length%2;
+	var item = bracketsConfig.toString().split(",");
+
+	var one = 0;
+
+	for(var i = 0; i< item.length; i++){
+		for (var j = 0; j<str.length; j++){
+			if(str.charAt(j) == item[i]){
+				one++;
+			}
+		}
+	}
+	if (one%2 == 0){
+		return true;
+	}
 }
+
+
